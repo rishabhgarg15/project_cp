714d72008cb3f8ed523a54595f9e4491b429b39d
Garvitmodi <44116230+Garvitmodi@users.noreply.github.com>
2020-05-31 13:14:20 +0530

    Google Auth, Upload and Download of files

---
+++ googleauth/configure/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+        google: {
+            clientID: '949692547903-qt4338h7phgsao1f9q32qbdff7kiqonu.apps.googleusercontent.com',
+            clientSecret: 'vm-K2H1nD12wp-lvM-nA6dHj'
+        },
+        mongodb: {
+            dbURI: 'mongodb://localhost/googleauth2'
+        },
+        session: {
+            cookieKey: 'Garvitislearningwebd'
+        }
+};
